db7846b235f895e980796a1858e0408c666e97a2
Ingen-Ear <jesse.pruitt7@gmail.com>
2020-08-16 16:36:53 -0500

    initial commit

---
+++ src/ant_handler.adb
@@ -0,0 +1,9 @@
+package body Ant_Handler with SPARK_Mode => On
+is
+
+  function Do_Something (Text : String) return String
+  is begin
+    return Text;
+  end Do_Something;
+  
+end Ant_Handler;
---
+++ src/ant_handler.ads
@@ -0,0 +1,4 @@
+package Ant_Handler with SPARK_Mode => On
+is
+  function Do_Something (Text : String) return String;
+end Ant_Handler;
